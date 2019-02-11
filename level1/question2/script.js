82c650970ad2f05d2f6b06e3cad40f0b12061a31
bunniicat <44841275+bunniicat@users.noreply.github.com>
2019-02-11 22:52:59 +0100

    Add files via upload

--- level1/question2/script.js
+++ level1/question2/script.js
@@ -1,1 +1,24 @@
 //Console.log out elements in JSON file
+
+let questionTwo = {
+	"video": [{
+		"id": 12312412312,
+		"name": "Ecuaciones Diferenciales",
+		"url": "/video/math/edo/12312412312",
+		"author": {
+			"data": [{
+				"name_author": "Alejandro Morales",
+				"url": "/author/alejandro-morales",
+				"type": "master"
+			}]
+		}
+	}]
+}
+
+for (let i=0; i<questionTwo.video.length; i++) {
+    for (key in questionTwo.video[i]) {
+        if (questionTwo.video[i].hasOwnProperty(key)) {
+            console.log(questionTwo.video[i][key]);
+        }
+    }
+}
