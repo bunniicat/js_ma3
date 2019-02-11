6e71065b5548e62fff53c278538403f4ec1f3d85
bunniicat <44841275+bunniicat@users.noreply.github.com>
2019-02-11 22:52:12 +0100

    Add files via upload

--- level1/question1/script.js
+++ level1/question1/script.js
@@ -1,1 +1,14 @@
-//Use RegEx to validate form
+document.getElementById("submit").addEventListener("click", function(){
+    let inputFields = document.getElementsByTagName("input");
+
+    for(var i=0; i < inputFields.length; i++) {
+        let theInput = inputFields[i].value;
+        let tryPattern = new RegExp(inputFields[i].pattern);
+        let tryTest = tryPattern.test(theInput);
+        
+        if (tryTest === false){
+            console.log(inputFields[i].name + " is incorrect");
+        }
+
+    }
+})
